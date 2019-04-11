f048bfc3e50ec748643c13dd20ce8de0d58d526a
milano12345 <dmilano857@gmail.com>
2019-04-11 14:18:40 -0600

    finished adding characters

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,14 +1,4 @@
 // CODE here for your Lambda Classes
-const fred = new Instructor({
-    name: 'Fred',
-    location: 'Bedrock',
-    age: 37,
-    gender: 'male',
-    favLanguage: 'JavaScript',
-    specialty: 'Front-end',
-    catchPhrase: `Don't forget the homies`
-  });
-
 //Person/
 
 class Person {
@@ -18,7 +8,7 @@ class Person {
         this.location = attributes.location;
         this.gender = attributes.gender;
     }
-    speak () {
+    speak() {
         return `Hello my name is ${this.name}, I am from ${this.location}`
     }
 };
@@ -35,9 +25,9 @@ class Instructor extends Person {
         this.catchPhrase = childAttrs.catchPhrase;
     }
     demo(subject) {
-        return `Today we are learning about ${this.subject} where the subject is the param passed in.` 
-    }s
-    grade(student,subject) {
+        return `Today we are learning about ${this.subject} where the subject is the param passed in.`
+    } s
+    grade(student, subject) {
         console.log(`${this.student.name} receives a perfect score on ${this.subject}`)
     }
 }
@@ -78,3 +68,84 @@ class ProjectManagers extends Student {
         console.log(`${this.name} debugs ${student.name}'s code on ${this.subject}`)
     }
 }
+
+// Characters /
+
+// Instructors /
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+});
+
+const josh = new Instructor({
+    name: 'Josh',
+    location: 'The Dark Realm',
+    age: 35,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Full-stack',
+    catchPhrase: `Don't forget the banjo!`
+});
+
+const jim = new Instructor({
+    name: 'Jim',
+    location: 'The hyperbelt',
+    age: 90,
+    gender: 'male',
+    favLanguage: 'Css',
+    specialty: 'Front-end',
+    catchPhrase: `Clarity is key!`
+});
+
+const jill = new Instructor({
+    name: 'Jill',
+    location: 'Venus',
+    age: 33,
+    gender: 'female',
+    favLanguage: 'binary',
+    specialty: 'back-end',
+    catchPhrase: `01010010101001010`
+});
+
+// Students /
+
+const daniel = new Student({
+    previousBackground: 'Bartending',
+    className: 'Web19',
+    grade: 'A'
+});
+
+const michelle = new Student({
+    previousBackground: 'Childcare',
+    className: 'Web19',
+    grade: 'A'
+});
+
+const mike = new Student({
+    previousBackground: 'Nurse',
+    className: 'Web19',
+    grade: 'A'
+});
+
+// Project Managers/
+
+const cj = new ProjectManagers({
+    gradClassName: 'Web15',
+    favInstructor: 'Josh'
+})
+
+const sam = new ProjectManagers({
+    gradClassName: 'Web9',
+    favInstructor: 'Ryan'
+})
+
+const larry = new ProjectManagers({
+    gradClassName: 'Web12',
+    favInstructor: 'Josh'
+})
